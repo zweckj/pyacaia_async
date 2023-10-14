8773d9cec9bc111b5b82833253e078b2b64c37ee
Fabian Affolter <mail@fabian-affolter.ch>
2023-10-14 17:52:12 +0200

    Remove asyncio

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "Programming Language :: Python :: Implementation :: CPython",
     ],
     packages=setuptools.find_packages(),
-    install_requires=["bleak>=0.20.2", "asyncio>=3.4.3"],
+    install_requires=["bleak>=0.20.2"],
     package_data={
         "license": ["LICENSE"],
     },
